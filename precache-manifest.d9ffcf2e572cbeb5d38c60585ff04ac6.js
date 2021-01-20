df2d7bfc4082298a9c34a725994638ceb7138272
vovador <vovadorko@gmail.com>
2021-01-20 16:58:04 +0200

    Updates

---
+++ precache-manifest.d9ffcf2e572cbeb5d38c60585ff04ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180f07283c01210acd0eb9a91c278475",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa02e23918c0f313daf0",
+    "url": "./static/css/main.5f82fc4c.chunk.css"
+  },
+  {
+    "revision": "618645990787c54db796",
+    "url": "./static/js/2.9b61cc4f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9b61cc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa02e23918c0f313daf0",
+    "url": "./static/js/main.dcb2d50a.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
